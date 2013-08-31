32aea601b7239c7d476111e9de1701f7f2fae606
Gordon Kyle <grkyle@gmail.com>
2013-08-31 17:01:57 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,10 +3,16 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  var k = fs.readFileSync('./index.html','utf8');
+  response.send( k );
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+buf = new Buffer(256);
+console.log(buf.toString('utf8', 0, len));
 });
